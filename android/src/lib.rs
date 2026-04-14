6c628922ee2ce39e7f4c69dc92cf0fa1b3684704
Ilan Ahmed <131523828+Lazycoder744@users.noreply.github.com>
2026-04-14 20:46:48 +0100

    small changes

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -13,11 +13,13 @@ fn android_main(app: AndroidApp) {
     let asset_mgr = EmbedAssetManager::new();
 
     let xr_entry = unsafe { openxr::Entry::load() }.expect("Unable to load OpenXR");
-    xr_entry.initialize_android_loader().expect("Unable to initialize android loader");
+    xr_entry
+        .initialize_android_loader()
+        .expect("Unable to initialize android loader");
 
     // At the moment, use precompiled dynamic loader for OpenXR.
     // TODO: How to build it with cross-compiler?
-    
+
     let output = XROutput::new(xr_entry);
     let stats = Stats::new("");
     let main = Main::new(asset_mgr, output.get_info(), stats);
@@ -27,23 +29,18 @@ fn android_main(app: AndroidApp) {
     loop {
         // Poll android events.
 
-        app.poll_events(Some(Duration::from_secs(0)), |event| { 
-            match event {
-                PollEvent::Main(event) => {
-                    match event {
-                        MainEvent::InputAvailable => {
-                            let mut it = app.input_events_iter().unwrap();
-                            while it.next(|_| InputStatus::Unhandled) {
-                            }
-                        },
-                        MainEvent::TerminateWindow {..} => {
-                            terminate = true;
-                        },
-                        _ => (),
-                    }
-                },
+        app.poll_events(Some(Duration::from_secs(0)), |event| match event {
+            PollEvent::Main(event) => match event {
+                MainEvent::InputAvailable => {
+                    let mut it = app.input_events_iter().unwrap();
+                    while it.next(|_| InputStatus::Unhandled) {}
+                }
+                MainEvent::TerminateWindow { .. } => {
+                    terminate = true;
+                }
                 _ => (),
-            }
+            },
+            _ => (),
         });
 
         if terminate {
